0ad709f0681fad4db65e19a10e51dc7b50d3bdfb
Tran Giang Long <trangianglong4003@gmail.com>
2019-06-05 18:05:46 +0200

    Initial Commit

---
+++ HeightMapAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeightMapAR
+//
+//  Created by Tran Giang Long on 05.06.19.
+//  Copyright © 2019 Tran Giang Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
